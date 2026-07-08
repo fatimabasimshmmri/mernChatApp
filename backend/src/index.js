c7274b0a08b7ff0a214c54e6c83120c4bc387ddc
nam <fatima.basim.shmmri@gmail.com>
2026-07-09 01:47:48 +0300

    refactor index.js: reorganize imports and add static file serving for public directory

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,21 @@
-import dns from 'node:dns';
+import dns from 'node:dns'; // google public DNS servers
 dns.setServers(['8.8.8.8', '8.8.4.4']); 
+
 import express from "express"
 import cors from "cors"
-import { clerkMiddleware } from '@clerk/express'
+import User from "./models/user.model.js" //importing my user model to use in the endpoints
+import fs from "fs"
+import path from "path"
+
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
-import User from "./models/user.model.js"
+import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from "./lib/db.js"
 
 const app = express()
 const port = process.env.PORT
 const frontend_url = process.env.FRONT_URL
+const publicDir = path.join(process.cwd(), 'public') //path to the public directory
 
 // -----        middleware        -----
 app.use(express.json())
@@ -19,6 +24,15 @@ app.use(clerkMiddleware())
 
 
 // -----        endpoints         -----
+
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(publicDir, 'index.html'), (err) => next(err))
+    })
+}
+
 app.get("/health", (req,res)=>{
     res.status(200).json({ok: true})
 })
